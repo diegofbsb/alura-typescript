af66ad0732d763e7ee5afd4602b8f1fb57c74d28
diegofbsb <diego.fernando.santosbsb@gmail.com>
2020-04-24 02:36:15 -0300

    commit

---
+++ app/ts/app.ts
@@ -0,0 +1,8 @@
+const controller = new NegociacaoController();
+
+$('.form').submit(controller.adiciona.bind(controller));
+
+
+// document
+//     .querySelector('.form')
+//     .addEventListener('submit', controller.adiciona.bind(controller));
